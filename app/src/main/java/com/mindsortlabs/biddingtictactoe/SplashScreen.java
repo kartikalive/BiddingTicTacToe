b75c721a8fc89575080f24f74d8a0af7e2a36b0f
kartikalive <sharmakartik840@gmail.com>
2017-07-22 22:57:22 +0530

    Bug fixes, UI Updated

--- app/src/main/java/com/mindsortlabs/biddingtictactoe/SplashScreen.java
+++ app/src/main/java/com/mindsortlabs/biddingtictactoe/SplashScreen.java
@@ -11,7 +11,7 @@ import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
 
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
