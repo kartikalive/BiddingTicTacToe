6fbe4d1af6f33334c37ee280f71f5bf70376d35b
Rishabh <rishabhjainps@gmail.com>
2017-08-28 00:28:13 +0530

    Fixed AI bug and added description for Bidding AI

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Bidding Tic Tac Toe
+===================================================================================================
+
+
+ <h3> Developed By: KARTIK BHUTANI <br> RISHABH JAIN </h3>
--- app/src/main/java/com/mindsortlabs/biddingtictactoe/ai/BiddingTicTacToeAi.java
+++ app/src/main/java/com/mindsortlabs/biddingtictactoe/ai/BiddingTicTacToeAi.java
@@ -1,6 +1,5 @@
 package com.mindsortlabs.biddingtictactoe.ai;
 
-
 import android.support.v4.util.Pair;
 import android.util.Log;
 
@@ -9,25 +8,26 @@ import com.mindsortlabs.biddingtictactoe.log.LogUtil;
 import java.util.Random;
 import java.util.Vector;
 
+/**
+ *  Based on Richman Theory
+ *  Pre Computes bidding values for all position possible
+ *  Uses bitmask-DP to reduced time complexity
+ */
 public class BiddingTicTacToeAi {
 
-    double F[][];
-    //double Bid[][] ;
-    //Pair<Integer,Integer> Pairs[][];
-    double bid = 0;
-    int TOTAL_COINS = 200;
-    Pair<Integer, Integer> favoured_child;
-    int first;
+    private static final String LOG_TAG = BiddingTicTacToeAi.class.getSimpleName();
+    // Richman Value for all possible states
+    double RichmanValue[][];
+    double biddingValue = 0;
+    int totalCoins = 200 , first ;
+    Pair<Integer, Integer> favouredChild;
 
     public BiddingTicTacToeAi() {
-        F = new double[513][513];
-        //Bid = new double[1024][1024];
-
-        //Pairs = new Pair[1024][1024];
+        RichmanValue = new double[513][513];
 
         for (int i = 0; i < 513; i++) {
             for (int j = 0; j < 513; j++) {
-                F[i][j] = -1.00;
+                RichmanValue[i][j] = -1.00;
                 //Bid[i][j]=-1.00;
             }
         }
@@ -39,35 +39,41 @@ public class BiddingTicTacToeAi {
         }
         nextMove(board, 0, 0, 0, 0);
         if (LogUtil.islogOn()) {
-            Log.d("TAG1245", "complete.");
+            Log.d(LOG_TAG, "complete.");
         }
     }
 
+    /**
+     * Reverse the player 3 <-> 5
+     * 5 -> AI
+     * 3 -> Real player
+     */
     int rev(int player) {
         if (player == 3)
             return 5;
         else
             return 3;
-
     }
 
+    /**
+     * Returns 0,1 or 2
+     * 0 -> continue playing
+     * 1 -> AI wins
+     * 2 -> Other player wins
+     */
     int done(int board[][]) {
 
         for (int i = 0; i < 3; i++) {
             if (board[i][0] + board[i][1] + board[i][2] == 3 * first) {
                 return 1;
             }
-
             if (board[0][i] + board[1][i] + board[2][i] == 3 * first) {
                 return 1;
             }
-
         }
-
         if (board[0][0] + board[1][1] + board[2][2] == 3 * first) {
             return 1;
         }
-
         if (board[0][2] + board[1][1] + board[2][0] == 3 * first) {
             return 1;
         }
@@ -79,17 +85,13 @@ public class BiddingTicTacToeAi {
             if (board[i][0] + board[i][1] + board[i][2] == 3 * rev(first)) {
                 return 2;
             }
-
             if (board[0][i] + board[1][i] + board[2][i] == 3 * rev(first)) {
                 return 2;
             }
-
         }
-
         if (board[0][0] + board[1][1] + board[2][2] == 3 * rev(first)) {
             return 2;
         }
-
         if (board[0][2] + board[1][1] + board[2][0] == 3 * rev(first)) {
             return 2;
         }
@@ -111,35 +113,31 @@ public class BiddingTicTacToeAi {
         return 0;
     }
 
-
+    /**
+     * Return Richman value for that position
+     * Saves the final position and biddingValue in favouredChild and biddingValue
+     */
     double nextMove(int board[][], int depth, int firstPlayer, int secondPlayer, int check) {
 
         if (check == 0)
-            if (Math.abs(F[firstPlayer][secondPlayer] + 1.00) > 0.0001)
-                return F[firstPlayer][secondPlayer];
+            if (Math.abs(RichmanValue[firstPlayer][secondPlayer] + 1.00) > 0.0001)
+                return RichmanValue[firstPlayer][secondPlayer];
 
         Pair<Integer, Integer> sol = null;
-
-
         int p = done(board);
-
-    /*
-        p can return 0,1
-
-        p==0 --> continue play
-
-        p==1 --> other player wins
-
-        p==2 --> draw
-
-    */
+        /*
+         *   p can return 0,1
+         *   p==0 --> continue play
+         *   p==1 --> AI wins
+         *   p==2 --> Player Wins or Draw
+         */
         if (p > 0) {
             if (p == 2) {
                 return (1.00);
             }
-
-            if (p == 1)
+            if (p == 1) {
                 return (0.00);
+            }
         }
 
         int max_depth = 8;
@@ -175,33 +173,43 @@ public class BiddingTicTacToeAi {
                         Fmax = x;
                         max_depth = depth;
                     }
-
                 }
-
             }
-
         }
+
         if (depth == 0) {
-            favoured_child = sol;
-            bid = Math.abs(Fmax - Fmin) / 2;
+            favouredChild = sol;
+            biddingValue = Math.abs(Fmax - Fmin) / 2;
         }
-        //Pairs[firstPlayer][secondPlayer]=sol;
-        //Bid[firstPlayer][secondPlayer]=Math.abs(Fmax-Fmin)/2;
-        return F[firstPlayer][secondPlayer] = Math.abs(Fmax + Fmin) / 2;
 
-
+        return RichmanValue[firstPlayer][secondPlayer] = Math.abs(Fmax + Fmin) / 2;
     }
 
+    /**
+     * Converts position in 3*3 matrix into a integer
+     * Used in bitmasking storing all position in an integer
+     * For example -
+     *  (2,1) --> position 8 --> return 2^8
+     *
+     *  Positions in a Tic Tac Toe
+     *      1 2 3
+     *      4 5 6
+     *      7 8 9
+     *
+     */
     int getSingleValue(int i, int j) {
         return (int) Math.pow(2, i * 3 + j);
     }
 
+    /**
+     * Returns number of coins to biddingValue with position to place the mark
+     */
     public Pair<Integer, Pair<Integer, Integer>> getSolution(Vector<String> board, int mycoins, char player) {
 
-//        char player;
+
         if (LogUtil.islogOn()) {
-            Log.d("BOARD ::", "  " + board.toString());
-            Log.d("COINS ::", "  " + mycoins);
+            Log.d(LOG_TAG , "BOARD ::"+ "  " + board.toString());
+            Log.d(LOG_TAG , "COINS ::"+ "  " + mycoins);
         }
         if (mycoins == 0)
             return Pair.create(0, Pair.create(0, 0));
@@ -211,7 +219,7 @@ public class BiddingTicTacToeAi {
         //If player is X, I'm the first player.
         //If player is O, I'm the second player.
         //cin >> player;
-//        player = 'X';
+        //player = 'X';
 
         //Read the board now. The board is a 3x3 array filled with X, O or _.
         int count = 0;
@@ -236,75 +244,97 @@ public class BiddingTicTacToeAi {
             }
 
         }
-        //if(count==9){
-        //    Pair.create(32,Pair.create(1,1));
-        //}
-
 
         first = 5;
 
         int minBid = 0;
 
-        //if(Math.abs(F[firstPLayer][secondPlayer]+1.00)<0.0001)
-        if (isOpponentAboutToWin(boards, player)) {
+        //if(Math.abs(RichmanValue[firstPLayer][secondPlayer]+1.00)<0.0001)
+        if (isAiAboutToWin(boards, player)) {
 
-            minBid = TOTAL_COINS - mycoins + 1;
+            minBid = totalCoins - mycoins + 1;
+
+        }else if (isOpponentAboutToWin(boards, player)) {
+
+            minBid = totalCoins - mycoins + 1;
 
         } else {
             nextMove(boards, 0, firstPLayer, secondPlayer, 1);
 
-            //bid = Bid[firstPLayer][secondPlayer];
-            //favoured_child = Pairs[firstPLayer][secondPlayer];
+            //biddingValue = Bid[firstPLayer][secondPlayer];
+            //favouredChild = Pairs[firstPLayer][secondPlayer];
 
-            bid = bid * 200;
+            biddingValue = biddingValue * 200;
 
-            minBid = (int) bid;
+            minBid = (int) biddingValue;
             Random r = new Random();
             minBid = minBid + (r.nextBoolean() == true ? r.nextInt(3) : -r.nextInt(3));
 
         }
 
 
-        int opponentBid = TOTAL_COINS - mycoins;
+        int opponentBid = totalCoins - mycoins;
 
         minBid = Math.min(minBid, opponentBid + 1);
         minBid = Math.min(minBid, mycoins);
         if (LogUtil.islogOn()) {
-            Log.d("BIDDING ::", "  " + minBid);
-            // Log.d("CHILDRENS ::","  "+favoured_child.first +"    "+favoured_child.second);
+            Log.d(LOG_TAG , "BIDDING ::"+ "  " + minBid);
+            // Log.d(LOG_TAG , "CHILDRENS ::"+"  "+favouredChild.first +"    "+favouredChild.second);
         }
-        if (favoured_child == null) {
+        if (favouredChild == null) {
             return Pair.create(minBid, Pair.create(0, 0));
         }
-        return Pair.create(minBid, favoured_child);
+        return Pair.create(minBid, favouredChild);
     }
 
+    /**
+     *  Returns true if AI can win in one move
+     */
+    private boolean isAiAboutToWin(int[][] board, char player) {
+
+        boolean flag = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if (board[i][j] == 0) {
+
+                    board[i][j] = first;
+                    int temp_flag = done(board);
+
+                    if (temp_flag == 1) {
+                        favouredChild = Pair.create(i, j);
+                    }
+                    flag = flag || (temp_flag == 1);
+                    board[i][j] = 0;
+                }
+            }
+        }
+        return flag;
+    }
+
+    /**
+     *  Returns true if player can win in one move
+     */
     private boolean isOpponentAboutToWin(int[][] board, char player) {
 
         boolean flag = false;
         for (int i = 0; i < 3; i++) {
-
             for (int j = 0; j < 3; j++) {
 
                 if (board[i][j] == 0) {
 
                     board[i][j] = rev(first);
-
                     int temp_flag = done(board);
 
                     if (temp_flag == 2) {
-                        favoured_child = Pair.create(i, j);
+                        favouredChild = Pair.create(i, j);
                     }
                     flag = flag || (temp_flag == 2);
-
                     board[i][j] = 0;
-
-
                 }
-
-
             }
         }
         return flag;
     }
+
 }
